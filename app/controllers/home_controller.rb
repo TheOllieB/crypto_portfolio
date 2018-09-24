5ee9acb6fe9c6880f77f428be01fe1b23239ad6f
Ollie B <oliver.burge2014@gmail.com>
2018-09-24 10:58:44 +0100

    Created search form for currency and connected to api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,31 @@ class HomeController < ApplicationController
   def index
   	require 'net/http'
   	require 'json'
-  	@url ='https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10'
+  	@url ='https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=100'
   	@uri = URI(@url)
   	@response = Net::HTTP.get(@uri)
   	@coins = JSON.parse(@response)
   	@my_coins= ["BTC","XRP","ADA","XLM", "STEEM"]
   end
   def about
+  end
+
+  def search
+  	require 'net/http'
+  	require 'json'
+  	@url ='https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=100'
+  	@uri = URI(@url)
+  	@response = Net::HTTP.get(@uri)
+  	@search_coin = JSON.parse(@response)
+
+  	@symbol = params[:sym]
+  	if @symbol 
+  		@symbol = @symbol.upcase
+  	end
+
+  	if @symbol == ""
+  		@symbol = "Please enter a Currency!"
+  	end
+
   end
 end
