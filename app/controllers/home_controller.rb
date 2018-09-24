df8e563ca9561f8e4737008bdac47e3e34d0acb0
Ollie B <oliver.burge2014@gmail.com>
2018-09-24 09:59:19 +0100

    Implemented CoinMarketCap api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+  	require 'net/http'
+  	require 'json'
+  	@url ='https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10'
+  	@uri = URI(@url)
+  	@response = Net::HTTP.get(@uri)
+  	@coins = JSON.parse(@response)
+  	@my_coins= ["BTC","XRP","ADA","XLM", "STEEM"]
   end
   def about
   end
